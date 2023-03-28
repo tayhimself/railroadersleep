bfc473ffb4d4ec09bf3bae1cdbca3736af878488
Salim Qadri <sqadri@rics.bwh.harvard.edu>
2023-03-28 17:45:12 -0400

    Layout improvements

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -1,6 +1,6 @@
 const getNormalizedPost = async (post) => {
 	const { frontmatter, Content, file } = post;
-	const ID = file.split('/').pop().split('.').shift();
+	const ID = file.split("/").pop().split(".").shift();
 
 	return {
 		id: ID,
@@ -24,22 +24,26 @@ const getNormalizedPost = async (post) => {
 		tags: frontmatter.tags,
 		readingTime: frontmatter.readingTime,
 
-    video_url: frontmatter.video_url,
-    video_title: frontmatter.video_title,
-    video_description: frontmatter.video_description,
-    video_duration: frontmatter.video_duration,
-    quote: frontmatter.quote,
-    quote_author: frontmatter.quote_author,
-    category: frontmatter.category,
-    section: frontmatter.section,
-    feature: frontmatter.feature,
+		video_url: frontmatter.video_url,
+		video_title: frontmatter.video_title,
+		video_description: frontmatter.video_description,
+		video_duration: frontmatter.video_duration,
+		quote: frontmatter.quote,
+		quote_author: frontmatter.quote_author,
+		category: frontmatter.category,
+		section: frontmatter.section,
+		feature: frontmatter.feature,
+		sort: frontmatter.sort,
 	};
 };
 
 const load = async function () {
-	const posts = import.meta.glob(['~/../data/blog/**/*.md', '~/../data/blog/**/*.mdx'], {
-		eager: true,
-	});
+	const posts = import.meta.glob(
+		["~/../data/blog/**/*.md", "~/../data/blog/**/*.mdx"],
+		{
+			eager: true,
+		}
+	);
 
 	const normalizedPosts = Object.keys(posts).map(async (key) => {
 		const post = await posts[key];
@@ -61,25 +65,26 @@ export const fetchPosts = async () => {
 	return await _posts;
 };
 
-
 /**
  * @param {string} section name
-*/
+ */
 export const fetchCategories = async (section) => {
-  const categories = import.meta.glob(['~/../data/categories/**/*.md', '~/../data/categories/**/*.mdx'], {
-    eager: true,
-  });
-  const normalizedCategories = Object.keys(categories).map(async (key) => {
-    const category = await categories[key];
-    return await getNormalizedPost(category);
-  });
-  // now filter out the ones that don't have a category after normalization
-  const results = (await Promise.all(normalizedCategories))
-    .filter((post) => post.category)
-    .filter((post) => post.section === section);
-  return results;
+	const categories = import.meta.glob(
+		["~/../data/categories/**/*.md", "~/../data/categories/**/*.mdx"],
+		{
+			eager: true,
+		}
+	);
+	const normalizedCategories = Object.keys(categories).map(async (key) => {
+		const category = await categories[key];
+		return await getNormalizedPost(category);
+	});
+	// now filter out the ones that don't have a category after normalization
+	const results = (await Promise.all(normalizedCategories))
+		.filter((post) => post.category)
+		.filter((post) => post.section === section);
+	return results;
 };
-
 
 /** */
 export const findPostsByIds = async (ids) => {
@@ -98,6 +103,10 @@ export const findPostsByIds = async (ids) => {
 /** */
 export const fetchPostsByFeature = async (feature) => {
 	let posts = await fetchPosts();
-  posts = posts.filter((post) => post.feature === feature);
-  return posts;
+	posts = posts
+		.filter((post) => post.feature === feature)
+    .sort((a, b) => {
+			return a.sort - b.sort;
+		});
+	return posts;
 };
